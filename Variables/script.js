846b5b0d2bfd49606fc5c876a2f938c17f3bff70
harishgowda84 <harishgowda84@gmail.com>
2021-12-24 16:51:42 +0530

    Variable Rules and assignment

---
+++ Variables/script.js
@@ -0,0 +1,32 @@
+/*
+
+This will show case variables defining rules
+
+1. Variables can contain letters,digits,underscore,$
+2. It cannot start with a number
+3. Variable should not be a keyword
+
+*/
+
+let customer = "Harish";
+
+//let 123_test="hello"; //invalid
+
+// let switch="yes";  //invalid
+
+// let @letter ="hi"; // invalid
+
+// variables are case sensitive
+
+//use camelCase or underscore
+
+let _user = "Tom";
+
+let $value = "one";
+
+console.log(customer);
+console.log(_user);
+console.log($value);
+
+let userName = "John Doe";
+let _username = "John Doe";
