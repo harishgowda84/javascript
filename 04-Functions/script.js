bacae681a67525a092db17aa2c1134b9dd51d105
harishgowda84 <harishgowda84@gmail.com>
2021-12-26 18:07:29 +0530

    Basic Functions

---
+++ 04-Functions/script.js
@@ -0,0 +1,65 @@
+/*
+Function declaration and
+*/
+
+function greeting() {
+  console.log("Hi from all");
+}
+
+greeting();
+
+/*
+  Function parameters
+*/
+
+function sum(a, b) {
+  console.log(`Sum is ${a + b}`);
+}
+
+sum(12, 45);
+
+/*
+  Return values
+*/
+
+function getMessage() {
+  return "This is a message";
+}
+
+let msg = getMessage();
+
+console.log(msg);
+
+/*
+ Function expressions
+
+ We can create function expressions by assigning function to a variable
+*/
+
+let multiply = function mul(m, n) {
+  return m * n;
+};
+
+console.log(multiply(10, 15));
+
+let m = function getList(element) {
+  console.log(this.title + " " + element);
+};
+
+/*
+    Callback functions have global scopes
+*/
+
+let video = {
+  title: "Josh",
+  tags: ["Music", "Romantic"],
+  printVideos() {
+    this.tags.forEach(
+      function (element) {
+        console.log(this.title + " : " + element);
+      }.bind(this)
+    );
+  },
+};
+
+video.printVideos();
