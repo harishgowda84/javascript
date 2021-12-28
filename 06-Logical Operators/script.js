75df9b455775cbe24dbf0e0a09dcafac0090f7db
harishgowda84 <harishgowda84@gmail.com>
2021-12-28 16:39:42 +0530

    Logical statements

---
+++ 06-Logical Operators/script.js
@@ -0,0 +1,25 @@
+/*
+  logical AND &&
+*/
+
+console.log("true && true", true && true);
+console.log("true && false", true && false);
+console.log("false && true", false && true);
+console.log("false && false", false && false);
+
+console.log("Cat && Dog: ", "Cat" && "Dog");
+console.log("false && Dog: ", false && "Dog");
+console.log("Dog && false: ", "Dog" && false);
+
+/*
+  logical OR ||
+*/
+
+console.log("true || true", true || true);
+console.log("true || false", true || false);
+console.log("false || true", false || true);
+console.log("false || false", false || false);
+
+console.log("Cat || Dog: ", "Cat" || "Dog");
+console.log("false || Dog: ", false || "Dog");
+console.log("Dog || false: ", "Dog" || false);
