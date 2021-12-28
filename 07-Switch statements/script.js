3ce713180adbfea4ff0171fa48136a64db2501db
harishgowda84 <harishgowda84@gmail.com>
2021-12-28 16:48:34 +0530

    Switch statements

---
+++ 07-Switch statements/script.js
@@ -0,0 +1,24 @@
+const dice = 5;
+
+switch (dice) {
+  case 1:
+    console.log("You rolled one");
+    break;
+  case 2:
+    console.log("You rolled two");
+    break;
+  case 3:
+    console.log("You rolled three");
+    break;
+  case 4:
+    console.log("You rolled four");
+    break;
+  case 5:
+    console.log("You rolled five");
+    break;
+  case 6:
+    console.log("You rolled six");
+    break;
+  default:
+    console.log("You rolled wrong one.Not supported");
+}
